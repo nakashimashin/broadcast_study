43391516e79969a40f365a44a5e4515b4a5a108b
nakashimashin <nakashimashin0517@gmail.com>
2024-09-30 23:34:22 +0900

    ゴールチン作成

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
 
 func main() {
-  fmt.Printf("Hello world")
+	fmt.Println("what is today's lucky number?")
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		getLuckyNum()
+	}()
+
+	wg.Wait()
+}
+
+func getLuckyNum() {
+	fmt.Println("...")
+
+	// 占いにかかる時間はランダム
+	rand.Seed(time.Now().Unix())
+	time.Sleep(time.Duration(rand.Intn(3000)) * time.Millisecond)
+
+	num := rand.Intn(10)
+	fmt.Printf("Today's your lucky number is %d!\n", num)
 }
