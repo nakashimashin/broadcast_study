151afdc091cb13f9840e70fd452e1de0bbbb4979
nakashimashin <nakashimashin0517@gmail.com>
2024-10-12 23:28:58 +0900

    chat機能作成(サーバ)

--- backend/go.mod
+++ backend/go.mod
@@ -5,15 +5,16 @@ go 1.22
 require (
 	github.com/cenkalti/backoff/v4 v4.1.1 // indirect
 	github.com/desertbit/timer v0.0.0-20180107155436-c41aec40b27f // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/improbable-eng/grpc-web v0.15.0 // indirect
 	github.com/klauspost/compress v1.11.7 // indirect
 	github.com/rs/cors v1.7.0 // indirect
-	golang.org/x/net v0.26.0 // indirect
-	golang.org/x/sys v0.21.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
-	google.golang.org/grpc v1.66.0 // indirect
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
+	google.golang.org/grpc v1.67.1 // indirect
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1 // indirect
-	google.golang.org/protobuf v1.34.1 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 	nhooyr.io/websocket v1.8.6 // indirect
 )
--- backend/server/server.go
+++ backend/server/server.go
@@ -1,1 +1,90 @@
 package main
+
+import (
+	pb "broadcast_study/pkg/grpc"
+	"log"
+	"net"
+	"os"
+	"sync"
+
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+    pb.UnimplementedChatRoomServer // 追加
+    clients map[string]pb.ChatRoom_ChatServer
+    mu      sync.RWMutex
+}
+
+func (s *server) addClient(uid string, srv pb.ChatRoom_ChatServer) {
+    s.mu.Lock()
+    defer s.mu.Unlock()
+    s.clients[uid] = srv
+}
+
+func (s *server) removeClient(uid string) {
+    s.mu.Lock()
+    defer s.mu.Unlock()
+    delete(s.clients, uid)
+}
+
+func (s *server) getClients() []pb.ChatRoom_ChatServer {
+    var cs []pb.ChatRoom_ChatServer
+    s.mu.RLock()
+    defer s.mu.RUnlock()
+    for _, c := range s.clients {
+        cs = append(cs, c)
+    }
+    return cs
+}
+
+func (s *server) Chat(srv pb.ChatRoom_ChatServer) error {
+    uid := uuid.Must(uuid.NewRandom()).String()
+    log.Printf("new user: %s", uid)
+
+    // 接続クライアントに登録
+    s.addClient(uid, srv)
+
+    // 関数を抜ける時はリストから削除
+    defer s.removeClient(uid)
+
+    defer func() {
+        if err := recover(); err != nil {
+            log.Printf("panic: %v", err)
+            os.Exit(1)
+        }
+    }()
+
+    for {
+        resp, err := srv.Recv()
+        if err != nil {
+            log.Printf("error: %v", err)
+            break
+        }
+        log.Printf("broadcast: %v", resp.Message)
+        for _, ss := range s.getClients() {
+            if err := ss.Send(&pb.ChatResponse{Message: resp.Message}); err != nil {
+                log.Printf("error: %v", err)
+            }
+        }
+    }
+    return nil
+}
+
+func main() {
+    addr := ":50051"
+    lis, err := net.Listen("tcp", addr)
+    if err != nil {
+        log.Fatalf("failed to listen: %v", err)
+    }
+    s := grpc.NewServer()
+    pb.RegisterChatRoomServer(s, &server{
+        clients: make(map[string]pb.ChatRoom_ChatServer),
+        mu:      sync.RWMutex{},
+    })
+
+    if err := s.Serve(lis); err != nil {
+        log.Fatalf("failed to serve: %v", err)
+    }
+}
